f84e177be3877ed476447039d3e4a49376881501
tom <t.fellinger@netzgestaltung.at>
2014-06-01 22:44:15 +0200

    included initializr options file, cleaned up

--- sidebar-content-first.php
+++
@@ -1,4 +0,0 @@
-<aside id="contentFirstBar" class="sidebar">
-  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : // begin secondary sidebar widgets ?>
-  <?php endif; // end primary sidebar widgets  ?>
-</aside>
